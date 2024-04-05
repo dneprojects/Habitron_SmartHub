ffdf5170f6c414e757afe6ec63460827727f867e
dneprojects <124430822+dneprojects@users.noreply.github.com>
2024-04-05 18:42:09 +0200

    Update start.sh

--- smart_hub/start.sh
+++ smart_hub/start.sh
@@ -1,9 +1,9 @@
 echo SUPERVISOR_TOKEN > /src/def_token.set
 echo ohne > def_token.set
-echo root /def_token.set
-echo root_src /src/def_token.set
-echo dot_src ./def_token.set
-echo dot ./def_token.set
-echo dot_dot ../def_token.set
+echo root > /def_token.set
+echo root_src > /src/def_token.set
+echo dot_src > ./def_token.set
+echo dot > ./def_token.set
+echo dot_dot > ../def_token.set
 printenv SUPERVISOR_TOKEN
 python smarthub.py
