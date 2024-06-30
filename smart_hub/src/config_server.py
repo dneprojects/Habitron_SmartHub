107056d3df3b7de76a30d24d9ef012d08c541a72
dneprojects <124430822+dneprojects@users.noreply.github.com>
2024-06-30 19:20:08 +0200

    Update config_server.py

--- smart_hub/src/config_server.py
+++ smart_hub/src/config_server.py
@@ -389,15 +389,7 @@ class ConfigServer:
     async def show_doc(request: web.Request) -> web.Response:  # type: ignore
         with open(WEB_FILES_DIR + DOC_FILE, "rb") as doc_file:
             pdf_content = doc_file.read()
-        return web.Response(
-            headers=MultiDict(
-                {
-                    "Content-Disposition": f"Attachment; filename = {DOC_FILE}",
-                    "Content-Type": "application/octet-stream",
-                }
-            ),
-            body=pdf_content,
-        )
+        return web.Response(body=pdf_content, content_type="application/pdf")
 
     @routes.get(path="/show_setup_doc")
     async def show_setup_doc(request: web.Request) -> web.Response:  # type: ignore
